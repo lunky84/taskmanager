f755de84b59e3ab01dcc2c0db1a9a5e633a1a891
John Downie <john.downie@thekeysupport.com>
2022-05-31 20:59:43 +0100

    Add active link state

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <Menu pointing secondary>
-      <Menu.Item>
+      <Menu.Item active={router.pathname == "/"}>
         <Link href="/">
           <a>Home</a>
         </Link>
       </Menu.Item>
-      <Menu.Item>
+      <Menu.Item active={router.pathname == "/tasks"}>
         <Link href="/tasks">
           <a>Tasks</a>
         </Link>
