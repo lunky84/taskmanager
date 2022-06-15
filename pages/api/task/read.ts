f71f68a31fed8c79e1ffc882c923da802cd24407
John Downie <john.downie@thekeysupport.com>
2022-06-15 13:30:02 +0100

    feat(sort): add basic sort to api

--- pages/api/task/read.ts
+++ pages/api/task/read.ts
@@ -3,14 +3,19 @@ import { Prisma } from "@prisma/client";
 
 export default async (req, res) => {
   const query = req.query;
-  const { page } = query;
+  const { page, sort } = query;
   try {
     const tasks: Prisma.TaskUncheckedCreateInput[] = await prisma.task.findMany({
       skip: (parseInt(page, 10) - 1) * 4,
       take: 4,
+      orderBy: [
+        {
+          title: sort ? sort : "asc",
+        },
+      ],
     });
     res.status(200).json(tasks);
   } catch (error) {
-    res.status(400).json({ message: "Something went wrong!" + page });
+    res.status(400).json({ message: "Something went wrong!" });
   }
 };
