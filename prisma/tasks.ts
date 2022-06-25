33274af2271e3892e7b930d1963159a9033350a7
John Downie <johnldownie@googlemail.com>
2022-06-25 19:20:07 +0100

    Update tasks seed data

--- prisma/tasks.ts
+++ prisma/tasks.ts
@@ -2,25 +2,37 @@ export const tasks = [
   {
     title: "Clean kitchen",
     description: "Put dishwasher on",
+    status: "Pending",
+    priority: 2,
   },
   {
     title: "Paint house",
     description: "Buy paint from B&Q",
+    status: "Active",
+    priority: 3,
   },
   {
     title: "Tidy living room",
     description: "Put things away and hoover",
+    status: "Pending",
+    priority: 2,
   },
   {
     title: "Arrange photos",
     description: "Frame pictures",
+    status: "Pending",
+    priority: 2,
   },
   {
     title: "Design bathroom",
     description: "Create a floor plan",
+    status: "Pending",
+    priority: 2,
   },
   {
     title: "Watch stranger things",
     description: "Take a break and watch some netflix",
+    status: "Pending",
+    priority: 2,
   },
 ];
