398612398c4ae2a05b5fa1d5a65cb011cfc01a90
John Downie <johnldownie@googlemail.com>
2022-06-25 20:58:37 +0100

    Implement example pie chart

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import { Prisma } from "@prisma/client";
 import { fetcher } from "../utils/fetcher";
 import prisma from "../lib/prisma";
 import { useState } from "react";
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
 
 export async function getServerSideProps() {
   const users: Prisma.UserUncheckedCreateInput[] = await prisma.user.findMany();
@@ -14,11 +17,40 @@ export async function getServerSideProps() {
   };
 }
 
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const options = [
   { key: "d", text: "DEVELOPER", value: "DEVELOPER" },
   { key: "u", text: "USER", value: "USER" },
   { key: "a", text: "ADMIN", value: "ADMIN" },
 ];
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 export default function Home({ initialUsers }) {
   const [users, setUsers] = useState<Prisma.UserUncheckedCreateInput[]>(initialUsers);
@@ -145,6 +177,7 @@ export default function Home({ initialUsers }) {
             ))}
           </Table.Body>
         </Table>
+        <Pie data={data} />
       </Container>
     </>
   );
