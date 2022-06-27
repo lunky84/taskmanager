e9f097c8e1cbcb4508cca15cef0438e27fbcc4f7
John Downie <johnldownie@googlemail.com>
2022-06-27 21:11:29 +0100

    Display created and modified dates

--- pages/task/[task_id].tsx
+++ pages/task/[task_id].tsx
@@ -2,6 +2,7 @@ import { Container } from "semantic-ui-react";
 import { NextSeo } from "next-seo";
 import { fetcher } from "../../utils/fetcher";
 import TaskForm from "@/components/Tasks/TaskForm";
+import { format } from 'date-fns'
 
 export async function getServerSideProps({ query: { task_id } }) {
   const task = await fetcher(`/api/task/read?task_id=${task_id}`, null);
@@ -20,7 +21,9 @@ export default function Task(props) {
         <NextSeo title="Task" description="The tasks page" />
 
         <h1>{props.task.title}</h1>
-        {props.task.task_id}
+        <p><strong>ID:</strong> {props.task.task_id}</p>
+        <p><strong>Created:</strong> { format(new Date(props.task.createAt), 'yyyy-MM-dd hh:mm') }</p>
+        <p><strong>Modified:</strong> { format(new Date(props.task.updatedAt), 'yyyy-MM-dd hh:mm') }</p>
 
         <TaskForm task={props.task}></TaskForm>
 
