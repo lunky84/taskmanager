ad0ffe02a6c9498023949dc41923386f79bfb015
John Downie <johnldownie@googlemail.com>
2022-07-09 23:53:28 +0100

    Add copy task ID to clipboard button

--- pages/task/[task_id].tsx
+++ pages/task/[task_id].tsx
@@ -1,8 +1,8 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import TaskForm from "@/components/Tasks/TaskForm";
 import { format } from 'date-fns';
 import { NextSeo } from "next-seo";
-import { Container } from "semantic-ui-react";
+import { Container, Icon, Button, Popup } from "semantic-ui-react";
 import { fetcher } from "../../utils/fetcher";
 
 export const getServerSideProps = async (context: any) => {
@@ -29,13 +29,44 @@ interface props{
 
 const TaskPage:FC<props> = (props) => {
 
+  const timeoutLength = 2500
+
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  let timeout: any = null;
+
+  const handleOpen = () => {
+    setIsOpen(true);
+    timeout = setTimeout(() => {
+      setIsOpen(false);
+    }, timeoutLength)
+  }
+  
+  const handleClose = () => {
+    setIsOpen(false)
+    clearTimeout(timeout)
+  }
+
+
   return (
     <>
       <Container style={{ margin: 20 }}>
         <NextSeo title="Task" description="The tasks page" />
 
         <h1>{props.task.title}</h1>
-        <p><strong>ID:</strong> {props.task.task_id}</p>
+
+        <Popup 
+          content='Copied to clipboard' 
+          on='click' 
+          inverted 
+          open={isOpen}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          trigger={<Button icon='copy' onClick={() => {navigator.clipboard.writeText(props.task.task_id)}} />} 
+        />
+
+        <p><strong>ID:</strong> <span onClick={() => {navigator.clipboard.writeText(props.task.task_id)}}>{props.task.task_id}</span></p>
         <p><strong>Created:</strong> { format(new Date(props.task.createdAt), 'yyyy-MM-dd hh:mm') }</p>
         <p><strong>Modified:</strong> { format(new Date(props.task.updatedAt), 'yyyy-MM-dd hh:mm') }</p>
 
