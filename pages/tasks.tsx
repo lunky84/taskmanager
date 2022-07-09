66eed28f97afeb593d04bee259c52331ef8374af
John Downie <johnldownie@googlemail.com>
2022-07-09 21:56:38 +0100

    Add button to reset search and filters

--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -110,144 +110,166 @@ export default function Tasks(props: any) {
     });
   }
 
+  const resetForm = async () => {
+    console.log("form reset");
+    setConfig({
+      ...config,
+      page: 1,
+      order: "title",
+      sort: "asc",
+      priority: "all",
+      status: "all",
+      search: "",
+    });
+  }
+
   return (
-    <Container style={{ margin: 20 }}>
-      <NextSeo title="Tasks" description="The tasks page" />
+    <div>
+      <Container style={{ margin: 20 }}>
+        <NextSeo title="Tasks" description="The tasks page" />
 
-      <h1>Tasks</h1>
+        <h1>Tasks</h1>
 
-      <Form onSubmit={async () => {
-        setConfig({
-          ...config,
-          order: "title",
-          sort: "asc",
-          priority: "all",
-          status: "all",
-          search: search,
-          page: 1
-        });
-      }}
-    >
-        <Form.Group widths='equal'>
-          <Form.Input
-            action={{
-              color: 'primary',
-              icon: 'search',
-            }}
-            label="Search"
-            placeholder="Enter a task ID or title"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <Form.Select
-            label="Priority"
-            value={config.priority}
-            options={priorityOptions}
-            onChange={(e: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
-              filterResults("priority", data.value as string);
-            }}
-          />
-          <Form.Select
-            label="Status"
-            value={config.status}
-            options={statusOptions}
-            onChange={(e: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
-              filterResults("status", data.value as string);
-            }}
-          />
-        </Form.Group>
-      </Form>
-
-      <br />
-
-      <Grid>
-        <Grid.Column width={12}></Grid.Column>
-        <Grid.Column width={4}>
-          <Form>
+        <Form onSubmit={async () => {
+          setConfig({
+            ...config,
+            order: "title",
+            sort: "asc",
+            priority: "all",
+            status: "all",
+            search: search,
+            page: 1
+          });
+        }}
+      >
+          <Form.Group widths='equal'>
+            <Form.Input
+              action={{
+                color: 'primary',
+                icon: 'search',
+              }}
+              label="Search"
+              placeholder="Enter a task ID or title"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
             <Form.Select
-              label="Results per page"
-              value={config.perPage}
-              options={[
-                { text: "5", value: "5" },
-                { text: "10", value: "10" },
-                { text: "25", value: "25" },
-                { text: "50", value: "50" },
-                { text: "100", value: "100" }
-              ]}
+              label="Priority"
+              value={config.priority}
+              options={priorityOptions}
               onChange={(e: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
-                setConfig({
-                  ...config,
-                  perPage: data.value,
-                  page: 1
-                })
+                filterResults("priority", data.value as string);
               }}
             />
-          </Form>
-        </Grid.Column>
-      </Grid>
+            <Form.Select
+              label="Status"
+              value={config.status}
+              options={statusOptions}
+              onChange={(e: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
+                filterResults("status", data.value as string);
+              }}
+            />
+          </Form.Group>
+        </Form>
 
-      <Table sortable celled>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell
-              sorted={config.order === "title" ? (config.sort === "asc" ? "ascending" : "descending") : undefined}
-              onClick={() => sorting("title")}
-            >
-              Title
-            </Table.HeaderCell>
-            <Table.HeaderCell>Description</Table.HeaderCell>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-            <Table.HeaderCell>Priority</Table.HeaderCell>
-            <Table.HeaderCell
-              sorted={config.order === "createdAt" ? (config.sort === "asc" ? "ascending" : "descending") : undefined}
-              onClick={() => sorting("createdAt")}
-            >Created</Table.HeaderCell>
-            <Table.HeaderCell collapsing>Action</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+        <Button basic icon labelPosition='left' onClick={resetForm}>
+          <Icon name='refresh' />
+          Reset
+        </Button>
 
-        <Table.Body>
-          {tasks.map((t, index) => (
-            <Table.Row key={index}>
-              <Table.Cell>
-                <Link href={`/task/${t.task_id}`}>
-                  <a>{t.title}</a>
-                </Link>
-              </Table.Cell>
-              <Table.Cell>{t.description}</Table.Cell>
-              <Table.Cell>{t.status}</Table.Cell>
-              <Table.Cell>{t.priority}</Table.Cell>
-              <Table.Cell>{ format(new Date(t.createdAt as string), 'yyyy-MM-dd') }</Table.Cell>
-              <Table.Cell>
-                <Button
-                  animated="fade"
-                  color="red"
-                  onClick={async () => {
-                    await fetcher("/api/task/delete", { task_id: t.task_id });
-                    await setTasks(tasks.filter((task) => task !== t));
-                  }}
-                >
-                  <Button.Content visible>Delete</Button.Content>
-                  <Button.Content hidden>
-                    <Icon name="delete" />
-                  </Button.Content>
-                </Button>
-              </Table.Cell>
+      </Container>
+
+      <Container style={{ margin: 20 }}>
+
+        <Grid columns={2} stackable>
+          <Grid.Column width={12}></Grid.Column>
+          <Grid.Column width={4}>
+            <Form>
+              <Form.Select
+                label="Results per page"
+                value={config.perPage}
+                options={[
+                  { text: "5", value: "5" },
+                  { text: "10", value: "10" },
+                  { text: "25", value: "25" },
+                  { text: "50", value: "50" },
+                  { text: "100", value: "100" }
+                ]}
+                onChange={(e: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
+                  setConfig({
+                    ...config,
+                    perPage: data.value,
+                    page: 1
+                  })
+                }}
+              />
+            </Form>
+          </Grid.Column>
+        </Grid>
+
+        <Table sortable celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell
+                sorted={config.order === "title" ? (config.sort === "asc" ? "ascending" : "descending") : undefined}
+                onClick={() => sorting("title")}
+              >
+                Title
+              </Table.HeaderCell>
+              <Table.HeaderCell>Description</Table.HeaderCell>
+              <Table.HeaderCell>Status</Table.HeaderCell>
+              <Table.HeaderCell>Priority</Table.HeaderCell>
+              <Table.HeaderCell
+                sorted={config.order === "createdAt" ? (config.sort === "asc" ? "ascending" : "descending") : undefined}
+                onClick={() => sorting("createdAt")}
+              >Created</Table.HeaderCell>
+              <Table.HeaderCell collapsing>Action</Table.HeaderCell>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+          </Table.Header>
 
-      <Pagination
-        boundaryRange={0}
-        ellipsisItem={null}
-        firstItem={null}
-        lastItem={null}
-        siblingRange={1}
-        totalPages={pageCount}
-        activePage={config.page}
-        onPageChange={(event: React.MouseEvent<HTMLAnchorElement>, data: PaginationProps) => pagginationHandler(data.activePage as number)}
-      />
-    </Container>
+          <Table.Body>
+            {tasks.map((t, index) => (
+              <Table.Row key={index}>
+                <Table.Cell>
+                  <Link href={`/task/${t.task_id}`}>
+                    <a>{t.title}</a>
+                  </Link>
+                </Table.Cell>
+                <Table.Cell>{t.description}</Table.Cell>
+                <Table.Cell>{t.status}</Table.Cell>
+                <Table.Cell>{t.priority}</Table.Cell>
+                <Table.Cell>{ format(new Date(t.createdAt as string), 'yyyy-MM-dd') }</Table.Cell>
+                <Table.Cell>
+                  <Button
+                    animated="fade"
+                    color="red"
+                    onClick={async () => {
+                      await fetcher("/api/task/delete", { task_id: t.task_id });
+                      await setTasks(tasks.filter((task) => task !== t));
+                    }}
+                  >
+                    <Button.Content visible>Delete</Button.Content>
+                    <Button.Content hidden>
+                      <Icon name="delete" />
+                    </Button.Content>
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+
+        <Pagination
+          boundaryRange={0}
+          ellipsisItem={null}
+          firstItem={null}
+          lastItem={null}
+          siblingRange={1}
+          totalPages={pageCount}
+          activePage={config.page}
+          onPageChange={(event: React.MouseEvent<HTMLAnchorElement>, data: PaginationProps) => pagginationHandler(data.activePage as number)}
+        />
+      </Container>
+    </div>
   );
 }
