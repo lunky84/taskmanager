39bf8bacf4a17ae45631d269d67d683106354eff
John Downie <johnldownie@googlemail.com>
2022-07-14 21:14:40 +0100

    Add TaskItem component

---
+++ components/Tasks/TaskItem.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import Link from "next/link";
+import { format } from "date-fns";
+import { Icon, Button, Table } from "semantic-ui-react";
+
+const TaskItem: FC<any> = ({ t, onClickDelete }) => {
+  return (
+    <Table.Row>
+      <Table.Cell>
+        <Link href={`/task/${t.task_id}`}>
+          <a>{t.title}</a>
+        </Link>
+      </Table.Cell>
+      <Table.Cell>{t.description}</Table.Cell>
+      <Table.Cell>{t.status}</Table.Cell>
+      <Table.Cell>{t.priority}</Table.Cell>
+      <Table.Cell>
+        {format(new Date(t.createdAt as string), "yyyy-MM-dd")}
+      </Table.Cell>
+      <Table.Cell>
+        <Button
+          animated="fade"
+          color="red"
+          onClick={() => {
+            onClickDelete(t);
+          }}
+        >
+          <Button.Content visible>Delete</Button.Content>
+          <Button.Content hidden>
+            <Icon name="delete" />
+          </Button.Content>
+        </Button>
+      </Table.Cell>
+    </Table.Row>
+  );
+};
+
+export default TaskItem;
--- components/Tasks/TaskList.tsx
+++ components/Tasks/TaskList.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
-import Link from "next/link";
-import { format } from "date-fns";
-import { Icon, Button, Table } from "semantic-ui-react";
+import { Table } from "semantic-ui-react";
+import TaskItem from "@/components/Tasks/TaskItem";
 
 const TaskList: FC<any> = ({ onClickSort, onClickDelete, config, tasks }) => {
   return (
@@ -45,33 +44,7 @@ const TaskList: FC<any> = ({ onClickSort, onClickDelete, config, tasks }) => {
 
       <Table.Body>
         {tasks.map((t: any, index: number) => (
-          <Table.Row key={index}>
-            <Table.Cell>
-              <Link href={`/task/${t.task_id}`}>
-                <a>{t.title}</a>
-              </Link>
-            </Table.Cell>
-            <Table.Cell>{t.description}</Table.Cell>
-            <Table.Cell>{t.status}</Table.Cell>
-            <Table.Cell>{t.priority}</Table.Cell>
-            <Table.Cell>
-              {format(new Date(t.createdAt as string), "yyyy-MM-dd")}
-            </Table.Cell>
-            <Table.Cell>
-              <Button
-                animated="fade"
-                color="red"
-                onClick={() => {
-                  onClickDelete(t);
-                }}
-              >
-                <Button.Content visible>Delete</Button.Content>
-                <Button.Content hidden>
-                  <Icon name="delete" />
-                </Button.Content>
-              </Button>
-            </Table.Cell>
-          </Table.Row>
+          <TaskItem key={index} t={t} onClickDelete={onClickDelete}></TaskItem>
         ))}
       </Table.Body>
     </Table>
