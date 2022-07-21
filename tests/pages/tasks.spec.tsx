afc1b906e56fc2640a411bfd6cfe4ed2d81ca21b
John Downie <johnldownie@googlemail.com>
2022-07-21 20:46:48 +0100

    Add tasks page test for displaying results

--- tests/pages/tasks.spec.tsx
+++ tests/pages/tasks.spec.tsx
@@ -55,9 +55,54 @@ describe("The tasks page", () => {
 
     render(<Tasks {...props} />);
 
-    expect(screen.getByTestId("no-results-message")).toBeInTheDocument();
-    expect(screen.getByTestId("no-results-message")).toHaveTextContent(
+    const noResultsMessage = screen.getByTestId("no-results-message");
+    expect(noResultsMessage).toBeInTheDocument();
+    expect(noResultsMessage).toHaveTextContent(
       "Sorry no results match your search criteria"
+    );
+  });
+
+  it("Should display results", async () => {
+    const props = {
+      initialTasks: [
+        {
+          task_id: "1d0dd84f-03ba-43e7-9f49-f2ba3034b7a7",
+          title:
+            "alias aut officia consequatur culpa sit repudiandae quia sed at",
+          description:
+            "Libero debitis rerum ut dicta in voluptatem natus aut dolores. Sint cum molestiae quaerat est ipsam aperiam odio. Dolorem sed nemo quibusdam cum quo culpa. Fuga ea nulla rerum ut. Alias et exercitationem voluptas.",
+          author_id: null,
+          status: "Active",
+          priority: 2,
+          date_due: null,
+          createdAt: "2022-06-28T16:55:50.107Z",
+          updatedAt: "2022-07-14T19:56:59.610Z",
+        },
+      ],
+      taskCount: 1,
+      config: {
+        page: "1",
+        order: "title",
+        sort: "asc",
+        priority: "all",
+        status: "all",
+        search: "",
+        perPage: "10",
+      },
+    };
+
+    render(<Tasks {...props} />);
+
+    screen.debug();
+
+    expect(screen.queryByTestId("no-results-message")).toBeNull();
+
+    const resultCount = screen.getByTestId("result-count");
+    expect(resultCount).toBeInTheDocument();
+    expect(resultCount).toHaveTextContent(props.taskCount.toString());
+
+    expect(screen.queryAllByRole("row")).toHaveLength(
+      props.initialTasks.length + 1
     );
   });
 });
