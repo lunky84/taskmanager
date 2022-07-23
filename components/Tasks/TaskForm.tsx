8b309c40c30d23f5bf495112ac6898cd79e115b3
John Downie <johnldownie@googlemail.com>
2022-07-23 22:29:28 +0100

    refactor: TaskForm

--- components/Tasks/TaskForm.tsx
+++ components/Tasks/TaskForm.tsx
@@ -1,13 +1,12 @@
 import { FC } from "react";
 import Link from "next/link";
-import { Form, Button, FormComponent } from "semantic-ui-react";
+import { Form, Button } from "semantic-ui-react";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { Prisma } from "@prisma/client";
 import { fetcher } from "../../utils/fetcher";
 import SemanticDatepicker from "react-semantic-ui-datepickers";
 import "react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css";
-import { DropdownProps } from "semantic-ui-react/dist/commonjs/modules/Dropdown/Dropdown";
 
 interface props {
   task: {
@@ -23,25 +22,11 @@ interface props {
 const TaskForm: FC<props> = (props) => {
   const [title, setTitle] = useState(props.task.title);
   const [description, setDescription] = useState(props.task.description);
-  const [status, setStatus] = useState<any | null>(props.task.status);
+  const [status, setStatus] = useState<string>(props.task.status);
   const [priority, setPriority] = useState<any | null>(props.task.priority);
   const [dateDue, setDateDue] = useState(props.task.date_due);
   const router = useRouter();
-  const onChange = (event: any, data: any) => setDateDue(data.value);
 
-  const statusOptions = [
-    { text: "Pending", value: "Pending" },
-    { text: "Active", value: "Active" },
-    { text: "Completed", value: "Completed" },
-  ];
-
-  const priorityOptions = [
-    { text: "Low", value: 1 },
-    { text: "Medium", value: 2 },
-    { text: "High", value: 3 },
-  ];
-
-  // const {form: Form} = props;
   return (
     <div>
       <Form
@@ -79,22 +64,27 @@ const TaskForm: FC<props> = (props) => {
         />
         <SemanticDatepicker
           label="Date due"
-          onChange={onChange}
+          onChange={(e, data: any) => setDateDue(data.value)}
           value={dateDue === null ? null : new Date(dateDue)}
         />
         <Form.Select
           label="Status"
           value={status}
-          options={statusOptions}
-          onChange={(
-            e: React.SyntheticEvent<HTMLElement>,
-            data: DropdownProps
-          ) => setStatus(data.value)}
+          options={[
+            { text: "Pending", value: "Pending" },
+            { text: "Active", value: "Active" },
+            { text: "Completed", value: "Completed" },
+          ]}
+          onChange={(e, data: any) => setStatus(data.value)}
         />
         <Form.Select
           label="Priority"
           value={priority}
-          options={priorityOptions}
+          options={[
+            { text: "Low", value: 1 },
+            { text: "Medium", value: 2 },
+            { text: "High", value: 3 },
+          ]}
           onChange={(e, data) => setPriority(data.value)}
         />
 
